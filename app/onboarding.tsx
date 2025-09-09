b94d724e7fc265a543454aca36fac8e028093f89
dima217 <dimapikulik217@mail.ru>
2025-09-09 22:39:38 +0300

    Refactor of splash screen

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -2,18 +2,24 @@ import { ThemedText } from "@/components/ui/ThemedText";
 import VideoSlider from "@/components/ui/video/videoSlider";
 import React from "react";
 import { View } from "react-native";
+import Cutlery from '../assets/images/Cutlery.svg';
 
 const onboardingScreen = () => {
     return (
         <View>
             <VideoSlider/>
-            <ThemedText type="title">
-                Culinary Constructor
-            </ThemedText>
+            <View>
+                <Cutlery/>
+                <ThemedText type="title">
+                    Culinary Constructor
+                </ThemedText>
+            </View>
+
             <ThemedText type="title">
                 A Sound Mind
                 In a Sound Body
             </ThemedText>
+
             <ThemedText type="subtitle">
                 Track your daily dietary norms without much effort
             </ThemedText>
