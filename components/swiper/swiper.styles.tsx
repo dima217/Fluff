17f2104bd85bdf2054d7b19d03948e107d409b72
dima217 <dimapikulik217@mail.ru>
2025-09-10 13:48:31 +0300

    Adding base swiper component

---
+++ components/swiper/swiper.styles.tsx
@@ -0,0 +1,34 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const SWIPER_WIDTH = Dimensions.get('window').width * 0.8; 
+const PADDING = 5;
+
+export const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+    },
+    swiperBar: {
+      width: SWIPER_WIDTH,
+      height: 70,
+      backgroundColor: '#1A1A1A',
+      borderRadius: 35,
+      justifyContent: 'flex-start',
+      padding: PADDING,
+    },
+    labelContainer: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    labelText: {
+      color: 'white',
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginRight: 10,
+    },
+});
--- components/swiper/swiper.tsx
+++ components/swiper/swiper.tsx
@@ -1,9 +1,71 @@
+import ArrowRight from '@/assets/images/ArrowRight.svg';
+import { Animated, Dimensions, Text, View } from "react-native";
+import { Gesture } from "react-native-gesture-handler";
+import { useAnimatedStyle, useSharedValue, withSpring, withTiming } from "react-native-reanimated";
+import Circle from "../ui/circle";
+import { styles } from './swiper.styles';
+
 export interface SwiperProps {
     label: string;
+    onSwipeEnd: () => void;
 }
 
-const Swiper = ( { label }: SwiperProps) => {
+const SWIPER_WIDTH = Dimensions.get('window').width * 0.8; 
+const CIRCLE_SIZE = 60;
+const PADDING = 5;
+const THRESHOLD = SWIPER_WIDTH - CIRCLE_SIZE - PADDING * 2
+
+const Swiper = ( { label, onSwipeEnd }: SwiperProps) => {
+   const isPressed = useSharedValue(false);
+
+   const translateX = useSharedValue(0);
+   
+   const panGesture = Gesture.Pan()
+    .onBegin(() => {
+      isPressed.value = true;
+    })
+    .onUpdate((e) => {
+      const newTranslateX = e.translationX;
+      translateX.value = Math.min(Math.max(0, newTranslateX), THRESHOLD);
+    })
+    .onEnd(() => {
+      if (translateX.value > THRESHOLD) {
+        onSwipeEnd();
+        translateX.value = withTiming(0);
+      } else {
+        translateX.value = withSpring(0);
+      }
+    })
     
+    const animatedCircleStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: translateX.value }],
+        }
+    });
+
+    const animatedLabelStyle = useAnimatedStyle(() => {
+        const opacity = 1 - translateX.value / THRESHOLD;
+        return {
+            opacity: opacity,
+        };
+    });
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.swiperBar}>
+                <Animated.View style={animatedCircleStyle}>
+                    <Circle
+                        svg={<ArrowRight/>}
+                        gesture={panGesture}
+                    />
+                </Animated.View>
+                <Animated.View style={[styles.labelContainer, animatedLabelStyle]}>
+                    <Text style={styles.labelText}>{label}</Text>
+                </Animated.View>
+            </View>
+        </View>
+    )
 }
 
 export default Swiper;
+
