78d4649116d66d9462bab18a9cd2293312fddb55
dima217 <dimapikulik217@mail.ru>
2025-09-11 10:03:09 +0300

    Adding custom button ui

---
+++ components/ui/common/button/button.styles.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+      paddingVertical: 12,
+      paddingHorizontal: 24,
+      borderRadius: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginVertical: 8,
+      height: 50,
+    },
+    text: {
+      fontSize: 16,
+      fontFamily: 'Inter-SemiBold',
+    },
+    content: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    iconWrapper: {
+      marginRight: 8,
+    },
+});
