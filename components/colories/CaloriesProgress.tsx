fe857d4e2ffaabb7d9d1309f71509c31927c1fee
dima217 <dimapikulik217@mail.ru>
2025-09-21 19:45:17 +0300

    Update expo sdk

--- components/colories/CaloriesProgress.tsx
+++ components/colories/CaloriesProgress.tsx
@@ -1,7 +1,7 @@
 import { AntDesign } from '@expo/vector-icons';
 import React, { useEffect, useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
-import ProgressBar from '../ui/progressBar/ProgressLine';
+import ProgressBar from '../ui/animated/progressBar/ProgressLine';
 import { styles } from './styles/progress.styles';
 
 interface CalorieProgressProps {
